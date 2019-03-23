fab463b16703ee17c9d426f22d3bb4354900f8fc
Geerocktricks <geerockface4@gmail.com>
2019-03-23 18:56:10 +0300

    This commit creates the home view function

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def home(request):
+    '''
+    view method that displays the home page
+    '''
+    return render(request , 'index.html')
